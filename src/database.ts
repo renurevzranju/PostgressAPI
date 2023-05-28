a0ab2d6c503a976949555e9da886f9eeef44d99f
Revathi <revathimeenashi@gmail.com>
2023-05-28 17:47:37 +0530

    created models and spec file to test the models

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,34 @@
 import dotenv from "dotenv";
 import { Pool } from "pg";
 
-dotenv.config()
+dotenv.config();
+"SET ENV=dev"
 
-const { POSTGRES_HOST, POSTGRES_DB ,POSTGRES_USER, POSTGRES_PASSWORD }  = process.env
+const {
+    POSTGRES_HOST,
+    POSTGRES_DB,
+    POSTGRES_TEST_DB,
+    ENV,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+} = process.env;
 
+let client;
+if (ENV === "test") {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    });
+}
 
-const client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD
-})
-
-export default client
+if (ENV === "dev") {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    });
+}
+export default client;
---
+++ src/models/mythical_weapon.ts
@@ -0,0 +1,65 @@
+import client from "../database";
+
+export type Weapon = {
+    id: Number;
+    name: String;
+    type: string;
+    weight: number;
+};
+
+export class MythicalWeaponStore {
+    async index(): Promise<Weapon[]> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = "SELECT * FROM mythical_weapons";
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows;
+        } catch (err) {
+            throw new Error(`Cannot get weapons ${err}`);
+        }
+    }
+
+    async show(id: string): Promise<Weapon> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = "SELECT * FROM mythical_weapons WHERE id=($1)";
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0];
+        } catch (err) {
+            throw new Error(`Could not find weapon ${id}. Error: ${err}`);
+        }
+    }
+
+    async create(w: Weapon): Promise<Weapon> {
+        try {
+            const sql =
+                "INSERT INTO mythical_weapons (name, type, weight) VALUES($1, $2, $3) RETURNING *";
+            // @ts-ignore
+            const conn = await client.connect();
+            const result = await conn.query(sql, [w.name, w.type, w.weight]);
+            const weapon = result.rows[0];
+            conn.release();
+            return weapon;
+        } catch (err) {
+            throw new Error(`Could not add new weapon ${w.name}. Error: ${err}`);
+        }
+    }
+
+    async delete(id: string): Promise<Weapon> {
+        try {
+            const sql = "DELETE FROM mythical_weapons WHERE id=($1)";
+            // @ts-ignore
+            const conn = await client.connect();
+            const result = await conn.query(sql, [id]);
+            const weapon = result.rows[0];
+            conn.release();
+            return weapon;
+        } catch (err) {
+            throw new Error(`Could not delete weapon ${id}. Error: ${err}`);
+        }
+    }
+}
